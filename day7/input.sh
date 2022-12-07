fe482b4ccff37775309a1adb1e5ee0bfc0954f39
Pål Nilsen <paal.nilsen@gmail.com>
2022-12-07 21:57:14 +0100

    Add day7

---
+++ day7/input.sh
@@ -0,0 +1,816 @@
+mkdir -p bqm
+mkdir -p ctztn
+mkdir -p dbclg
+mkdir -p fhndmnt
+mkdir -p gczqbh
+mkfile -n 276177b hvbf.lvm
+mkdir -p lnsgbqp
+mkdir -p pblb
+mkdir -p pwfs
+mkfile -n 209719b rtv.cjj
+mkfile -n 192236b vmtnnfv.mdq
+mkdir -p vmvpf
+mkdir -p wjgh
+mkdir -p wjqsq
+cd bqm
+mkfile -n 133711b vqv
+mkfile -n 263237b wwlv.vgv
+cd ..
+cd ctztn
+mkdir -p gpgbfzbs
+mkdir -p hfvqpt
+mkfile -n 55466b jshtffs
+mkdir -p rgzfgz
+mkfile -n 115519b wdzh.szq
+mkdir -p wmfvclz
+mkfile -n 221554b zgwwgps.gfn
+cd gpgbfzbs
+mkfile -n 354710b tlvmh.ghp
+cd ..
+cd hfvqpt
+mkdir -p cggfgt
+mkdir -p jshtffs
+mkfile -n 175230b nnn.chs
+mkfile -n 13406b plqjpqss
+mkdir -p pnwcq
+mkdir -p str
+cd cggfgt
+mkfile -n 109434b fhww
+mkdir -p glnrg
+mkdir -p mptpfvlh
+mkfile -n 305315b nlhfgpr.vnv
+mkfile -n 297519b tlvmh.ghp
+mkdir -p vmvpf
+cd glnrg
+mkfile -n 345026b qzfpwv
+mkfile -n 196361b rgzfgz
+cd ..
+cd mptpfvlh
+mkdir -p lsbjp
+mkfile -n 121174b vlbbbgnn
+cd lsbjp
+mkdir -p jqclpq
+mkfile -n 359939b nlhfgpr
+mkdir -p whp
+cd jqclpq
+mkfile -n 189319b mnhnclpt.qsv
+cd ..
+cd whp
+mkdir -p lgdlztb
+cd lgdlztb
+mkfile -n 94150b wdzh.szq
+cd ..
+cd ..
+cd ..
+cd ..
+cd vmvpf
+mkdir -p pfzd
+mkfile -n 66706b rgzfgz
+mkdir -p zrc
+cd pfzd
+mkfile -n 345595b bpjpm
+mkdir -p ljtdcgt
+mkdir -p plltpvgv
+cd ljtdcgt
+mkfile -n 134178b nlhfgpr
+cd ..
+cd plltpvgv
+mkfile -n 355576b rqj
+cd ..
+cd ..
+cd zrc
+mkfile -n 45363b vqv
+cd ..
+cd ..
+cd ..
+cd jshtffs
+mkdir -p bwbwldd
+mkfile -n 124375b fwvcbcpz.lvr
+mkdir -p jctctjj
+mkdir -p jshtffs
+mkdir -p sngchq
+cd bwbwldd
+mkfile -n 144330b vthsfg
+cd ..
+cd jctctjj
+mkdir -p flhrmt
+mkdir -p rgzfgz
+mkdir -p vmtnnfv
+cd flhrmt
+mkfile -n 132008b dgrt
+mkfile -n 263345b nlhfgpr.hss
+cd ..
+cd rgzfgz
+mkfile -n 214808b mcqlbzs.gwl
+mkfile -n 120157b nlhfgpr.hcs
+cd ..
+cd vmtnnfv
+mkdir -p rgzfgz
+mkfile -n 52978b vlbbbgnn
+cd rgzfgz
+mkfile -n 177061b nnc
+cd ..
+cd ..
+cd ..
+cd jshtffs
+mkfile -n 157488b bbsfrg.bbr
+mkfile -n 166412b dhwbvggg
+cd ..
+cd sngchq
+mkdir -p bvcsvv
+mkdir -p hhd
+cd bvcsvv
+mkdir -p gbmccf
+mkfile -n 5663b vmtnnfv
+mkfile -n 278760b vqv
+mkfile -n 323147b wjlm.cbw
+cd gbmccf
+mkdir -p fttnqbwp
+cd fttnqbwp
+mkdir -p fhs
+cd fhs
+mkfile -n 137718b rgzfgz
+cd ..
+cd ..
+cd ..
+cd ..
+cd hhd
+mkfile -n 350019b gphghhdh.rrb
+cd ..
+cd ..
+cd ..
+cd pnwcq
+mkdir -p qtqqdd
+cd qtqqdd
+mkfile -n 62319b tlvmh.ghp
+cd ..
+cd ..
+cd str
+mkfile -n 334780b tlvmh.ghp
+mkdir -p vcdcgbtr
+mkfile -n 211478b vvh.tzh
+mkdir -p zlsv
+cd vcdcgbtr
+mkfile -n 276438b zhh.dsw
+cd ..
+cd zlsv
+mkfile -n 315890b vmvpf.tcs
+cd ..
+cd ..
+cd ..
+cd rgzfgz
+mkdir -p qwzfs
+cd qwzfs
+mkdir -p lzl
+cd lzl
+mkdir -p gpgccbc
+mkfile -n 259493b vmvpf.dgz
+cd gpgccbc
+mkfile -n 124141b tlvmh.ghp
+cd ..
+cd ..
+cd ..
+cd ..
+cd wmfvclz
+mkfile -n 25819b fmqtjdfd.dwp
+mkfile -n 253619b qccd.gnd
+mkfile -n 121087b sjsz.rtg
+mkfile -n 350518b tgs
+mkdir -p zmft
+cd zmft
+mkfile -n 248086b vlbbbgnn
+cd ..
+cd ..
+cd ..
+cd dbclg
+mkdir -p dgm
+mkdir -p rwdfw
+mkfile -n 182454b slctrj
+mkfile -n 103919b tzp.dvf
+mkfile -n 311601b zlbmvfq.qsh
+cd dgm
+mkdir -p fflb
+mkdir -p jshtffs
+mkfile -n 26424b shzfvbs.jrj
+cd fflb
+mkfile -n 154495b ngzgfpt
+cd ..
+cd jshtffs
+mkfile -n 11509b fwvcbcpz.lvr
+mkfile -n 169064b hzhbhgjn.dvc
+mkfile -n 38675b jwcnwm.prf
+mkdir -p mjb
+mkdir -p nwwcb
+mkfile -n 300017b qtrqn.qdv
+mkdir -p rfjdh
+mkfile -n 173691b rgzfgz.wgz
+cd mjb
+mkdir -p rgzfgz
+cd rgzfgz
+mkfile -n 192387b ssm
+cd ..
+cd ..
+cd nwwcb
+mkfile -n 312609b ggvwvdw.hhg
+mkfile -n 107482b ppqmjzh
+mkfile -n 204253b vlbbbgnn
+cd ..
+cd rfjdh
+mkfile -n 305213b smf.jgv
+cd ..
+cd ..
+cd ..
+cd rwdfw
+mkdir -p hbsbctsr
+mkfile -n 145955b tqj
+mkdir -p vvthg
+cd hbsbctsr
+mkdir -p shqdr
+cd shqdr
+mkfile -n 101441b tlvmh.ghp
+cd ..
+cd ..
+cd vvthg
+mkfile -n 344816b wdzh.szq
+cd ..
+cd ..
+cd ..
+cd fhndmnt
+mkdir -p lstcq
+mkdir -p nvlncq
+mkdir -p rgzfgz
+mkdir -p ssqs
+mkdir -p vzpnjpsv
+cd lstcq
+mkfile -n 24507b jshtffs.nrf
+mkfile -n 34332b lvqdthm.gwh
+mkfile -n 313126b szfqhv.jtv
+mkfile -n 274515b wmwpglh
+cd ..
+cd nvlncq
+mkdir -p vmtnnfv
+cd vmtnnfv
+mkfile -n 104013b wdzh.szq
+cd ..
+cd ..
+cd rgzfgz
+mkfile -n 311945b nlhfgpr.fcv
+cd ..
+cd ssqs
+mkfile -n 104254b vlbbbgnn
+mkfile -n 207441b vmpd.tdt
+cd ..
+cd vzpnjpsv
+mkfile -n 253593b plfsrlr
+cd ..
+cd ..
+cd gczqbh
+mkdir -p nlhfgpr
+mkdir -p rgzfgz
+mkdir -p vmtnnfv
+cd nlhfgpr
+mkdir -p bwpd
+mkfile -n 86459b vmvpf.mwh
+cd bwpd
+mkfile -n 345948b rgzfgz.qnp
+cd ..
+cd ..
+cd rgzfgz
+mkdir -p rgzfgz
+mkdir -p sdsqqq
+cd rgzfgz
+mkdir -p mprdrmmz
+mkfile -n 331389b rbcrg.chs
+mkfile -n 191328b rpvpnprr.smn
+cd mprdrmmz
+mkdir -p ncljgf
+cd ncljgf
+mkfile -n 309996b bjwvhw.ltr
+cd ..
+cd ..
+cd ..
+cd sdsqqq
+mkfile -n 95157b qmwcb.wsm
+cd ..
+cd ..
+cd vmtnnfv
+mkdir -p gbntbhrj
+mkdir -p nlhfgpr
+mkfile -n 283985b ptl
+mkdir -p tnljzft
+mkfile -n 38026b vlbbbgnn
+cd gbntbhrj
+mkfile -n 117782b jshtffs.fpb
+mkfile -n 9101b rgzfgz.bdp
+mkfile -n 300155b vmtnnfv.wcq
+cd ..
+cd nlhfgpr
+mkdir -p cpglmt
+mkfile -n 65206b cwmpnz.czl
+mkdir -p gcrntzb
+cd cpglmt
+mkfile -n 288805b mghgq.msj
+cd ..
+cd gcrntzb
+mkfile -n 126442b tlvmh.ghp
+cd ..
+cd ..
+cd tnljzft
+mkfile -n 278719b ccwfvtgh
+cd ..
+cd ..
+cd ..
+cd lnsgbqp
+mkdir -p lqltnz
+mkdir -p vmtnnfv
+mkfile -n 69119b vmvpf.rgd
+cd lqltnz
+mkdir -p vmtnnfv
+mkdir -p whvdf
+cd vmtnnfv
+mkfile -n 3012b vmtnnfv.zlv
+cd ..
+cd whvdf
+mkdir -p nlhfgpr
+mkfile -n 115838b rwqbdmfb
+mkfile -n 298358b tlvmh.ghp
+mkdir -p vmtnnfv
+mkfile -n 335379b vmtnnfv.ltj
+mkfile -n 129665b zglmsgf
+cd nlhfgpr
+mkfile -n 12607b fwvcbcpz.lvr
+mkdir -p lrbrcz
+mkdir -p nvqzmflc
+mkfile -n 3793b tlvmh.ghp
+mkfile -n 39593b vqv
+cd lrbrcz
+mkfile -n 195996b vmtnnfv.hlb
+cd ..
+cd nvqzmflc
+mkfile -n 190198b vmvpf
+cd ..
+cd ..
+cd vmtnnfv
+mkfile -n 359558b rdjqs
+cd ..
+cd ..
+cd ..
+cd vmtnnfv
+mkdir -p clwrz
+mkfile -n 362269b nphz.plr
+mkdir -p rqbq
+mkfile -n 188843b tlvmh.ghp
+mkdir -p wsrdwm
+cd clwrz
+mkfile -n 233770b wdzh.szq
+cd ..
+cd rqbq
+mkfile -n 183770b jtnlfd
+mkfile -n 71164b lwgqzccd.dgb
+cd ..
+cd wsrdwm
+mkfile -n 266826b bqg
+mkdir -p nlhfgpr
+mkfile -n 53502b nlhfgpr.slz
+mkfile -n 320422b qzsfvzl.wbb
+mkdir -p vhq
+mkdir -p vmvpf
+cd nlhfgpr
+mkdir -p ddlplnhp
+mkdir -p nbdj
+cd ddlplnhp
+mkdir -p czp
+mkdir -p dmchbv
+mkdir -p nlj
+mkfile -n 299460b tlvmh.ghp
+mkdir -p wrqqln
+cd czp
+mkfile -n 286886b tlvmh.ghp
+mkfile -n 98020b vlln
+cd ..
+cd dmchbv
+mkfile -n 239186b ppjcgwq
+cd ..
+cd nlj
+mkfile -n 310042b nsgvvfcw.vfm
+mkfile -n 70206b vlbbbgnn
+cd ..
+cd wrqqln
+mkdir -p bdprgbp
+cd bdprgbp
+mkfile -n 42055b hjwrqzf.cqj
+cd ..
+cd ..
+cd ..
+cd nbdj
+mkdir -p cdjd
+cd cdjd
+mkfile -n 330601b bzhvfzh.ldp
+mkfile -n 14662b hjhdwz.qrt
+mkfile -n 188113b pct
+cd ..
+cd ..
+cd ..
+cd vhq
+mkfile -n 348147b qsgjjzn.nft
+cd ..
+cd vmvpf
+mkfile -n 192773b wdzh.szq
+cd ..
+cd ..
+cd ..
+cd ..
+cd pblb
+mkdir -p jshtffs
+mkdir -p pwgtvhm
+cd jshtffs
+mkdir -p vtthv
+cd vtthv
+mkfile -n 39542b fwfz.gll
+cd ..
+cd ..
+cd pwgtvhm
+mkfile -n 271449b hvfq
+cd ..
+cd ..
+cd pwfs
+mkdir -p dnnzr
+mkdir -p ncjzvsph
+mkdir -p nlhfgpr
+cd dnnzr
+mkfile -n 337565b vlbbbgnn
+mkfile -n 345668b vqv
+cd ..
+cd ncjzvsph
+mkfile -n 290438b fdtftjj.sdt
+mkfile -n 170963b tlvmh.ghp
+mkfile -n 56596b vlbbbgnn
+mkfile -n 72923b vmvpf.ldg
+cd ..
+cd nlhfgpr
+mkfile -n 321213b vqv
+cd ..
+cd ..
+cd vmvpf
+mkdir -p gwbt
+mkdir -p vmvpf
+mkdir -p vplm
+cd gwbt
+mkfile -n 209996b jrzlrp
+mkfile -n 23503b vmvpf.hwn
+mkfile -n 173320b vqv
+cd ..
+cd vmvpf
+mkfile -n 299874b hqwppnws
+mkfile -n 177211b pcqdrn.wqd
+mkfile -n 266011b pjllm
+mkfile -n 227969b wdzh.szq
+cd ..
+cd vplm
+mkdir -p fbl
+mkfile -n 354786b jdb
+mkfile -n 273253b nncwhbq.vzn
+mkdir -p rgzfgz
+cd fbl
+mkfile -n 38301b vqv
+mkfile -n 256284b wdzh.szq
+cd ..
+cd rgzfgz
+mkfile -n 60018b rdsrl.whb
+mkdir -p rgzfgz
+mkdir -p wgvw
+cd rgzfgz
+mkdir -p gmcqfqrn
+cd gmcqfqrn
+mkfile -n 337129b rgzfgz.pjs
+cd ..
+cd ..
+cd wgvw
+mkfile -n 14727b zdhwsm
+cd ..
+cd ..
+cd ..
+cd ..
+cd wjgh
+mkdir -p ccgddrt
+mkdir -p cqh
+mkfile -n 60305b dtjctmsw.qgp
+mkdir -p hwr
+mkdir -p jshtffs
+mkfile -n 335879b smfjq
+mkfile -n 344915b vlbbbgnn
+mkfile -n 63031b wdzh.szq
+mkdir -p zbmjg
+cd ccgddrt
+mkfile -n 148368b dpnlbg.fbb
+mkdir -p vmvpf
+cd vmvpf
+mkdir -p ptwrssn
+mkfile -n 340536b tlvmh.ghp
+cd ptwrssn
+mkfile -n 119875b jshtffs
+cd ..
+cd ..
+cd ..
+cd cqh
+mkdir -p blvcjl
+mkfile -n 158276b fjv
+mkfile -n 279848b fnh.rdf
+mkdir -p vmtnnfv
+cd blvcjl
+mkdir -p cgsznlm
+mkfile -n 47381b gtcv
+mkfile -n 227459b rgzfgz.wvr
+mkdir -p tgcqhcv
+mkdir -p vff
+mkdir -p vmtnnfv
+mkdir -p vmvpf
+mkfile -n 75937b vmvpf.psj
+mkfile -n 29291b wdzh.szq
+mkdir -p zqmsn
+cd cgsznlm
+mkfile -n 91350b rpmwrtm.dlr
+cd ..
+cd tgcqhcv
+mkdir -p hnrhdvff
+mkfile -n 81404b jshtffs.msp
+mkfile -n 158082b jshtffs.szt
+mkdir -p nhmqcm
+mkdir -p rvfsl
+mkdir -p smcrnh
+cd hnrhdvff
+mkfile -n 154674b htwms.nqc
+mkdir -p rslbgns
+mkfile -n 192997b vlbbbgnn
+mkdir -p vmvpf
+mkfile -n 230969b vqv
+mkfile -n 195769b ztfg.mfd
+cd rslbgns
+mkfile -n 236439b dwbjh.whr
+mkdir -p gzph
+mkfile -n 279480b tlvmh.ghp
+mkfile -n 107447b vqv
+cd gzph
+mkfile -n 46947b tdnmmqm
+cd ..
+cd ..
+cd vmvpf
+mkfile -n 285442b sftfzz.pzn
+mkfile -n 292269b vmtnnfv.bbc
+cd ..
+cd ..
+cd nhmqcm
+mkfile -n 219245b hwpgpdm.qrf
+mkfile -n 130169b rsmdgwjz.rph
+cd ..
+cd rvfsl
+mkfile -n 263768b ctprwbl.tjt
+mkdir -p jshtffs
+mkdir -p rgzfgz
+mkfile -n 291351b vpcd.hvf
+cd jshtffs
+mkfile -n 9465b fwvcbcpz.lvr
+mkdir -p htb
+mkdir -p jshtffs
+mkfile -n 202004b nrgq.cwj
+mkdir -p vmvpf
+cd htb
+mkfile -n 323938b gwhlttb
+mkfile -n 75918b llmlgmqz.zsp
+mkdir -p nvfv
+mkfile -n 117786b qjjv
+mkfile -n 167090b vmvpf.qvd
+cd nvfv
+mkfile -n 135820b tlvmh.ghp
+cd ..
+cd ..
+cd jshtffs
+mkfile -n 240326b fhr.jbf
+cd ..
+cd vmvpf
+mkfile -n 154309b fwvcbcpz.lvr
+cd ..
+cd ..
+cd rgzfgz
+mkdir -p jjfdn
+mkdir -p nlhfgpr
+cd jjfdn
+mkfile -n 46675b vfwhjfl.zrz
+cd ..
+cd nlhfgpr
+mkfile -n 26537b qwzcnfz
+cd ..
+cd ..
+cd ..
+cd smcrnh
+mkfile -n 331015b vpttwwc.jgr
+cd ..
+cd ..
+cd vff
+mkfile -n 68000b ntbfs.znj
+mkfile -n 265866b rgzfgz.lsl
+mkfile -n 53570b vmvpf.ncb
+mkfile -n 74229b vqv
+mkfile -n 96730b zwq.zdb
+cd ..
+cd vmtnnfv
+mkdir -p rgzfgz
+mkdir -p zmr
+cd rgzfgz
+mkfile -n 7492b fwvcbcpz.lvr
+mkdir -p gjg
+mkdir -p sfrlhmm
+cd gjg
+mkdir -p dqjfz
+mkfile -n 59560b vqv
+mkfile -n 33788b wdzh.szq
+cd dqjfz
+mkdir -p zhtbsq
+cd zhtbsq
+mkfile -n 222414b cgsjvqvd.rsd
+cd ..
+cd ..
+cd ..
+cd sfrlhmm
+mkfile -n 94423b rgzfgz
+cd ..
+cd ..
+cd zmr
+mkfile -n 269611b gvhrsmt.bdt
+mkdir -p vmtnnfv
+cd vmtnnfv
+mkdir -p rgzfgz
+cd rgzfgz
+mkfile -n 355703b fwvcbcpz.lvr
+mkfile -n 286378b pcf.dss
+cd ..
+cd ..
+cd ..
+cd ..
+cd vmvpf
+mkfile -n 336533b fwvcbcpz.lvr
+mkfile -n 80609b hsfnp
+mkfile -n 9548b jld.rvt
+mkdir -p jshtffs
+mkfile -n 219383b rjtrbjl.vbc
+mkdir -p vmtnnfv
+mkfile -n 182036b vmvpf
+cd jshtffs
+mkdir -p jshtffs
+mkdir -p rtnwnqcb
+mkfile -n 358722b szfhzcj.tjt
+mkfile -n 167284b tlvmh.ghp
+cd jshtffs
+mkfile -n 68702b blcgmn
+mkfile -n 299911b jjd.pvb
+mkfile -n 243135b wdzh.szq
+cd ..
+cd rtnwnqcb
+mkfile -n 128787b jmvlgd.bwv
+mkfile -n 268582b tdpqcqfs.nzp
+mkfile -n 348305b vmvpf.bnl
+cd ..
+cd ..
+cd vmtnnfv
+mkdir -p nlhfgpr
+mkfile -n 241246b tlvmh.ghp
+cd nlhfgpr
+mkfile -n 292694b tlvmh.ghp
+cd ..
+cd ..
+cd ..
+cd zqmsn
+mkfile -n 244052b mmthhf.wmd
+mkfile -n 144084b psspc.czf
+mkdir -p vflpgd
+cd vflpgd
+mkfile -n 22881b hsbbcdp.psd
+mkdir -p rgzfgz
+mkdir -p tmz
+mkfile -n 246706b vmvpf
+cd rgzfgz
+mkfile -n 346162b jshtffs.hqw
+mkdir -p lhmh
+mkdir -p pfwbz
+mkfile -n 87008b tzddth.whp
+cd lhmh
+mkdir -p nlhfgpr
+cd nlhfgpr
+mkfile -n 167305b tlvmh.ghp
+mkdir -p vnsdh
+cd vnsdh
+mkfile -n 22714b tlvmh.ghp
+cd ..
+cd ..
+cd ..
+cd pfwbz
+mkfile -n 163433b bzttjhqz.hpz
+mkfile -n 198604b vlbbbgnn
+cd ..
+cd ..
+cd tmz
+mkfile -n 300571b bmrs
+cd ..
+cd ..
+cd ..
+cd ..
+cd vmtnnfv
+mkdir -p llwmdf
+mkdir -p pvzdlsgp
+mkfile -n 48025b zvvrwh.dhw
+cd llwmdf
+mkfile -n 236755b hbfdll
+cd ..
+cd pvzdlsgp
+mkdir -p drjbqvn
+mkfile -n 102548b fwvcbcpz.lvr
+mkfile -n 300337b gcv.bnn
+mkdir -p grgc
+mkfile -n 27918b mtnvzzc.vlj
+mkdir -p nlhfgpr
+mkfile -n 92639b wdzh.szq
+mkfile -n 152813b wmnpg
+cd drjbqvn
+mkfile -n 292541b jshtffs.gjg
+mkfile -n 84918b vqv
+mkfile -n 355763b zqsmd
+cd ..
+cd grgc
+mkfile -n 45875b nlhfgpr.rnf
+mkfile -n 294562b slshp
+cd ..
+cd nlhfgpr
+mkfile -n 310430b fpglshq
+cd ..
+cd ..
+cd ..
+cd ..
+cd hwr
+mkdir -p jshtffs
+mkdir -p srrgj
+cd jshtffs
+mkfile -n 275253b fwvcbcpz.lvr
+mkfile -n 262142b vlqflmdn.gtd
+mkdir -p zrm
+cd zrm
+mkfile -n 103078b jshtffs.ncf
+cd ..
+cd ..
+cd srrgj
+mkfile -n 328221b fqdmfshj.ftn
+mkfile -n 107311b lpdrrw
+mkfile -n 214681b ltq.gmq
+mkfile -n 321175b pzd
+cd ..
+cd ..
+cd jshtffs
+mkdir -p ngbb
+cd ngbb
+mkfile -n 218170b lvmj.sjg
+mkfile -n 123373b nwv
+mkfile -n 326485b rgzfgz.gjd
+mkfile -n 192138b vlbbbgnn
+mkdir -p vmtnnfv
+mkfile -n 179027b vptgrfr
+cd vmtnnfv
+mkfile -n 206046b vnlsclvh
+cd ..
+cd ..
+cd ..
+cd zbmjg
+mkfile -n 30152b fwvcbcpz.lvr
+cd ..
+cd ..
+cd wjqsq
+mkfile -n 23196b cmvfcwp.nfz
+mkdir -p lnrzw
+mkfile -n 316798b nlhfgpr.spt
+mkdir -p ntrngh
+mkfile -n 284657b tlvmh.ghp
+mkfile -n 50758b vmvpf.msg
+cd lnrzw
+mkdir -p crc
+mkdir -p ddhnvwqt
+mkdir -p gdhfqlln
+mkdir -p vmtnnfv
+cd crc
+mkfile -n 296423b vmtnnfv.prq
+mkfile -n 26320b vqv
+cd ..
+cd ddhnvwqt
+mkfile -n 173923b vqv
+cd ..
+cd gdhfqlln
+mkfile -n 275288b vqv
+cd ..
+cd vmtnnfv
+mkfile -n 52004b pgtmmtrf.vfc
+cd ..
+cd ..
+cd ntrngh
+mkdir -p czt
+mkfile -n 185012b vlbbbgnn
+cd czt
+mkfile -n 355964b cvnslv
+mkfile -n 143166b mwbcbphd.prr
+mkfile -n 40666b rgzfgz
