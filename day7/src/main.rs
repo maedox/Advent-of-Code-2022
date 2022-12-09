e024a161b1e1540207d8c57876aeca1159e39bea
Pål Nilsen <paal.nilsen@gmail.com>
2022-12-09 18:14:37 +0100

    Refactor day 7 with id_tree

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,7 +1,5 @@
-use std::{cell::RefCell, fmt, rc::Rc};
-
 use camino::Utf8PathBuf;
-use indexmap::IndexMap;
+use id_tree::{InsertBehavior, Node, Tree};
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while1},
@@ -11,76 +9,6 @@ use nom::{
 };
 
 // Very much based on article by Fasterthanlime
-
-fn process(input: &str, part: u8) -> u64 {
-    let lines = input
-        .lines()
-        .map(|l| all_consuming(parse_line)(l).finish().unwrap().1);
-
-    let root = Rc::new(RefCell::new(Node::default()));
-    let mut node = root.clone();
-
-    for line in lines {
-        match line {
-            Line::Command(cmd) => match cmd {
-                Command::Ls => {
-                    // just ignore those
-                }
-                Command::Cd(path) => match path.as_str() {
-                    "/" => {
-                        // ignore, we're already there
-                    }
-                    ".." => {
-                        let parent = node.borrow().parent.clone().unwrap();
-                        node = parent;
-                    }
-                    _ => {
-                        let child = node.borrow_mut().children.entry(path).or_default().clone();
-                        node = child;
-                    }
-                },
-            },
-            Line::Entry(entry) => match entry {
-                Entry::Dir(dir) => {
-                    let entry = node.borrow_mut().children.entry(dir).or_default().clone();
-                    entry.borrow_mut().parent = Some(node.clone());
-                }
-                Entry::File(size, file) => {
-                    let entry = node.borrow_mut().children.entry(file).or_default().clone();
-                    entry.borrow_mut().size = size as usize;
-                    entry.borrow_mut().parent = Some(node.clone());
-                }
-            },
-        }
-    }
-
-    match part {
-        1 => all_dirs(root)
-            .map(|d| d.borrow().total_size())
-            .filter(|&s| s <= 100_000)
-            // .inspect(|s| {
-            // dbg!(s);
-            // })
-            .sum::<u64>(),
-        2 => {
-            let total_space = 70000000_u64;
-            let used_space = root.borrow().total_size();
-            let free_space = total_space.checked_sub(used_space).unwrap();
-            let needed_free_space = 30000000_u64;
-            let minimum_space_to_free = needed_free_space.checked_sub(free_space).unwrap();
-
-            all_dirs(root)
-                .map(|d| d.borrow().total_size())
-                .filter(|&s| s >= minimum_space_to_free)
-                // .inspect(|s| {
-                //     dbg!(s);
-                // })
-                .min()
-                .unwrap_or_default()
-        }
-        _ => unreachable!(),
-    }
-}
 
 fn parse_path(i: &str) -> IResult<&str, Utf8PathBuf> {
     map(
@@ -124,7 +52,6 @@ fn parse_command(i: &str) -> IResult<&str, Command> {
     let (i, _) = tag("$ ")(i)?;
     alt((map(parse_ls, Into::into), map(parse_cd, Into::into)))(i)
 }
-type NodeHandle = Rc<RefCell<Node>>;
 
 #[derive(Debug)]
 enum Entry {
@@ -155,77 +82,117 @@ fn parse_line(i: &str) -> IResult<&str, Line> {
     ))(i)
 }
 
-#[derive(Default)]
-struct Node {
-    size: usize,
-    children: IndexMap<Utf8PathBuf, NodeHandle>,
-    parent: Option<NodeHandle>,
+#[derive(Debug)]
+struct FsEntry {
+    path: Utf8PathBuf,
+    size: u64,
 }
 
-impl Node {
-    fn is_dir(&self) -> bool {
-        self.size == 0 && !self.children.is_empty()
+fn total_size(tree: &Tree<FsEntry>, node: &Node<FsEntry>) -> color_eyre::Result<u64> {
+    let mut total = node.data().size;
+    for child in node.children() {
+        total += total_size(tree, tree.get(child)?)?;
     }
-
-    fn total_size(&self) -> u64 {
-        self.children
-            .values()
-            .map(|child| child.borrow().total_size())
-            .sum::<u64>()
-            + self.size as u64
-    }
+    Ok(total)
 }
 
-struct PrettyNode<'a>(&'a NodeHandle);
-
-impl<'a> fmt::Debug for PrettyNode<'a> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let this = self.0.borrow();
-        if this.size == 0 {
-            writeln!(f, "(dir)")?;
-        } else {
-            writeln!(f, "(file, size={})", this.size)?;
-        }
-
-        for (name, child) in &this.children {
-            // not very efficient at all, but shrug
-            for (index, line) in format!("{:?}", PrettyNode(child)).lines().enumerate() {
-                if index == 0 {
-                    writeln!(f, "{name} {line}")?;
-                } else {
-                    writeln!(f, "  {line}")?;
-                }
-            }
-        }
-        Ok(())
-    }
-}
-
-fn all_dirs(n: NodeHandle) -> Box<dyn Iterator<Item = NodeHandle>> {
-    // clippy is wrong and should feel bad
-    #[allow(clippy::needless_collect)]
-    let children = n.borrow().children.values().cloned().collect::<Vec<_>>();
-
-    Box::new(
-        std::iter::once(n).chain(
-            children
-                .into_iter()
-                .filter_map(|c| {
-                    if c.borrow().is_dir() {
-                        Some(all_dirs(c))
-                    } else {
-                        None
-                    }
-                })
-                .flatten(),
-        ),
-    )
-}
-
-fn main() {
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install().unwrap();
     const INPUT: &str = include_str!("../input.txt");
-    println!("Part 1: {}", process(INPUT, 1));
-    println!("Part 2: {}", process(INPUT, 2));
+    println!("Part 1: {}", process(INPUT, 1)?);
+    println!("Part 2: {}", process(INPUT, 2)?);
+    Ok(())
+}
+
+fn process(input: &str, part: u8) -> color_eyre::Result<u64> {
+    let lines = input
+        .lines()
+        .map(|l| all_consuming(parse_line)(l).finish().unwrap().1);
+
+    let mut tree = Tree::<FsEntry>::new();
+    let root = tree.insert(
+        Node::new(FsEntry {
+            path: "/".into(),
+            size: 0,
+        }),
+        InsertBehavior::AsRoot,
+    )?;
+    let mut curr = root;
+
+    for line in lines {
+        println!("{line:?}");
+        match line {
+            Line::Command(cmd) => match cmd {
+                Command::Ls => {
+                    // just ignore those
+                }
+                Command::Cd(path) => match path.as_str() {
+                    "/" => {
+                        // ignore, we're already there
+                    }
+                    ".." => {
+                        curr = tree.get(&curr)?.parent().unwrap().clone();
+                    }
+                    _ => {
+                        let node = Node::new(FsEntry {
+                            path: path.clone(),
+                            size: 0,
+                        });
+                        curr = tree.insert(node, InsertBehavior::UnderNode(&curr))?;
+                    }
+                },
+            },
+            Line::Entry(entry) => match entry {
+                Entry::Dir(_) => {
+                    // ignore, we'll do that when we `cd` into them
+                }
+                Entry::File(size, path) => {
+                    let node = Node::new(FsEntry { size, path });
+                    tree.insert(node, InsertBehavior::UnderNode(&curr))?;
+                }
+            },
+        }
+    }
+
+    let mut s = String::new();
+    tree.write_formatted(&mut s)?;
+    println!("{s}");
+
+    match part {
+        1 => {
+            let sum = tree
+                .traverse_pre_order(tree.root_node_id().unwrap())?
+                // only consider folders:
+                .filter(|n| !n.children().is_empty())
+                .map(|n| total_size(&tree, n).unwrap())
+                .filter(|&s| s <= 100_000)
+                .inspect(|s| {
+                    dbg!(s);
+                })
+                .sum::<u64>();
+            Ok(dbg!(sum))
+        }
+        2 => {
+            let total_space = 70000000_u64;
+            let used_space = total_size(&tree, tree.get(tree.root_node_id().unwrap())?)?;
+            let free_space = total_space.checked_sub(dbg!(used_space)).unwrap();
+            let needed_free_space = 30000000_u64;
+            let minimum_space_to_free = needed_free_space.checked_sub(free_space).unwrap();
+
+            let size_to_remove = tree
+                .traverse_pre_order(tree.root_node_id().unwrap())?
+                .filter(|n| !n.children().is_empty())
+                .map(|n| total_size(&tree, n).unwrap())
+                .filter(|&s| s >= minimum_space_to_free)
+                .inspect(|s| {
+                    dbg!(s);
+                })
+                .min()
+                .unwrap_or_default();
+            Ok(dbg!(size_to_remove))
+        }
+        _ => unreachable!(),
+    }
 }
 
 #[cfg(test)]
@@ -233,43 +200,21 @@ mod tests {
     use super::*;
 
     const INPUT: &str = include_str!("../input.txt");
-    const TEST_INPUT: &str = "$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k";
+    const TEST_INPUT: &str = include_str!("../sample-input.txt");
 
     #[test]
     fn test_input() {
-        assert_eq!(process(TEST_INPUT, 1), 95437);
-        assert_eq!(process(TEST_INPUT, 2), 24933642);
+        assert_eq!(process(TEST_INPUT, 1).unwrap(), 95_437);
+        assert_eq!(process(TEST_INPUT, 2).unwrap(), 24_933_642);
     }
 
     #[test]
     fn part_1() {
-        assert_eq!(process(INPUT, 1), 1118405);
+        assert_eq!(process(INPUT, 1).unwrap(), 1_118_405);
     }
 
     #[test]
     fn part_2() {
-        assert_eq!(process(INPUT, 2), 12545514);
+        assert_eq!(process(INPUT, 2).unwrap(), 12_545_514);
     }
 }
