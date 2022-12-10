860df600b3459e7d5c52b1877cc0b8346bf620b6
Pål Nilsen <paal.nilsen@gmail.com>
2022-12-10 22:59:35 +0100

    Refactor day 10, add part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -1,66 +1,108 @@
-use crate::Operation::*;
+use itertools::{repeat_n, Itertools};
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{i32, line_ending},
+    multi::separated_list1,
+    Parser,
+};
+use nom_supreme::ParserExt;
+use std::{error::Error, iter, time::Instant};
+use Operation::*;
 
 const INPUT: &str = include_str!("../input.txt");
 
+type OutResult = std::result::Result<(), Box<dyn Error>>;
+type IResult<'a, T> = nom::IResult<&'a str, T>;
+
+#[derive(Debug, Clone, Copy)]
 enum Operation {
     Noop,
-    Add,
+    Add(i32),
 }
 
-fn parse_input(input: &str) -> (Operation, i64) {
-    if input.starts_with('n') {
-        (Noop, 0)
-    } else {
-        let num = input.split_whitespace().last().unwrap().parse().unwrap();
-        (Add, num)
-    }
+fn parse_operation(input: &str) -> IResult<Operation> {
+    alt((tag("noop").value(Noop), tag("addx ").precedes(i32).map(Add)))(input)
 }
 
-fn count_cycles(input: &str) -> i64 {
-    let mut x = 1;
-    let mut signal_strengths: Vec<i64> = vec![];
-    let mut lines = input.lines();
+type Parsed = Vec<Operation>;
 
-    let mut c: usize = 1;
-
-    loop {
-        check_signal(c, &mut signal_strengths, x);
-        if let Some(line) = lines.next() {
-            match parse_input(line) {
-                (Noop, 0) => {}
-                (Add, num) => {
-                    c += 1;
-                    check_signal(c, &mut signal_strengths, x);
-                    x += num;
-                }
-                _ => (),
-            }
-            c += 1;
-        } else {
-            check_signal(c, &mut signal_strengths, x);
-            break;
-        }
-    }
-    dbg!(signal_strengths.iter().sum())
+fn parse(data: &str) -> IResult<Parsed> {
+    separated_list1(line_ending, parse_operation)(data)
 }
 
-fn check_signal(c: usize, signal_strengths: &mut Vec<i64>, x: i64) {
-    if [20, 60, 100, 140, 180, 220].contains(&c) {
-        signal_strengths.push(c as i64 * x)
-    }
+fn iter_register(data: &Parsed) -> impl Iterator<Item = i32> + '_ {
+    data.iter()
+        .scan(1, |register, op| {
+            Some(repeat_n(
+                *register, // dereference before mutating
+                match op {
+                    Noop => 1,
+                    Add(x) => {
+                        *register += x;
+                        2
+                    }
+                },
+            ))
+        })
+        .flatten()
 }
 
-fn main() {
-    println!("Part 1: {}", count_cycles(INPUT));
+fn check_signal_strength(data: &Parsed) -> i32 {
+    iter_register(data)
+        .zip(1..)
+        .filter(|(_, cycle)| [20, 60, 100, 140, 180, 220].contains(cycle))
+        .map(|(reg_x, cycle)| reg_x * cycle)
+        .sum()
 }
+
+fn draw_pixels(data: &Parsed) -> String {
+    iter_register(data)
+        .chunks(40)
+        .into_iter()
+        .flat_map(|row| {
+            row.zip(0..)
+                .map(|(x, pos)| if x.abs_diff(pos) <= 1 { '#' } else { '.' })
+                .chain(iter::once('\n'))
+        })
+        .collect()
+}
+
+fn main() -> OutResult {
+    let duration = Instant::now();
+    let parsed = parse(INPUT)?.1;
+    println!("Part 1: {}", check_signal_strength(&parsed));
+    println!("Part 2:\n{}", draw_pixels(&parsed));
+    println!("Finished in {:?}", duration.elapsed());
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+    const INPUT: &str = include_str!("../input.txt");
     const TEST_INPUT: &str = include_str!("../sample-input.txt");
 
     #[test]
-    fn part_1() {
-        assert_eq!(count_cycles(TEST_INPUT), 13140);
-        assert_eq!(count_cycles(INPUT), 14040);
+    fn test_1() -> OutResult {
+        assert_eq!(check_signal_strength(&parse(TEST_INPUT)?.1), 13140);
+        assert_eq!(check_signal_strength(&parse(INPUT)?.1), 14040);
+        Ok(())
+    }
+
+    #[test]
+    fn part_2() -> OutResult {
+        assert_eq!(
+            draw_pixels(&parse(TEST_INPUT)?.1),
+            "\
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+"
+        );
+        Ok(())
     }
 }
